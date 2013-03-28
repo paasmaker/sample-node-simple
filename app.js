9bcb762dfbd83b95f621803a5d3aa80f3fa42e0e
Daniel Foote <freefoote@dview.net>
2013-03-28 10:02:35 +0800

    Initial checkin of example Node.js application.

---
+++ app.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var port_number = process.env.PM_PORT || 8000;
+
+http.createServer(function (req, res) {
+  var request = url.parse(req.url, true);
+  var action = request.pathname;
+
+  if (action == '/p-white.png') {
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    var img = fs.readFile('./p-white.png', function(e, data) {
+	  res.end(data, 'binary');
+	});
+  }
+  if (action == '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var img = fs.readFile('./index.html', function(e, data) {
+	  res.end(data, 'utf8');
+	});
+  }
+
+}).listen(port_number, function() {
+  console.log("Demo web server listening at http://locahost:" + port_number);
+});
+
